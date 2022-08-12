f4da1e1a478130d91e0a54e310b069337b945ba0
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-08-12 09:22:24 -0300

    feat: create editor object

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { useState } from "react";
+import { createEditor } from "slate";
+import { withReact } from "slate-react";
+
 export function App() {
+  const editor = useState(() => withReact(createEditor()));
+
   return <h1>Rich-Text Editor</h1>;
 }
