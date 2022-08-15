df22e3999ede8f0ad6c12336d3be268fef8f80c6
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-08-15 20:58:26 -0300

    feat: add quotes icon to quote block

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,8 +1,10 @@
+import { Quotes } from "phosphor-react";
 import { RenderElementProps } from "slate-react";
 
 export function Quote(props: RenderElementProps) {
   return (
     <blockquote {...props.attributes}>
+      <Quotes weight="fill" size={28} />
       <p>{props.children}</p>
     </blockquote>
   );
