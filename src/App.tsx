ee6e86edd59fa8a1310c4c50bc9487993ef8eb8a
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-08-17 17:57:09 -0300

    feat: add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,8 @@ export function App() {
   const [theme, setTheme] = useState("light");
   const [editor] = useState(() => withReact(createEditor()));
 
+  const isDarkTheme = theme === "dark";
+
   const initialValue: Descendant[] = useMemo(() => {
     const content = localStorage.getItem("@TextEditor:content");
 
@@ -171,99 +173,101 @@ export function App() {
     <>
       <Header theme={theme} onThemeChange={toggleTheme} />
 
-      <div className="container">
-        <main>
-          <div className="toolbar">
-            <button
-              title="Título (alt+t)"
-              onClick={() => CustomEditor.toggleTitleBlock(editor)}
+      <div className={`background ${isDarkTheme && "dark-theme"}`}>
+        <div className="container">
+          <main className={`${isDarkTheme && "dark-theme-main"}`}>
+            <div className={`toolbar ${isDarkTheme && "dark-theme-color"}`}>
+              <button
+                title="Título (alt+t)"
+                onClick={() => CustomEditor.toggleTitleBlock(editor)}
+              >
+                <TextH size={22} />
+                <span>Título</span>
+              </button>
+              <button
+                title="Negrito (alt+b)"
+                onClick={() => CustomEditor.toggleBoldMark(editor)}
+              >
+                <TextBolder size={22} />
+                <span>Negrito</span>
+              </button>
+              <button
+                title="Itálico (alt+i)"
+                onClick={() => CustomEditor.toggleItalicMark(editor)}
+              >
+                <TextItalic size={22} />
+                <span>Itálico</span>
+              </button>
+              <button
+                title="Sublinhado (alt+u)"
+                onClick={() => CustomEditor.toggleUnderlineMark(editor)}
+              >
+                <TextUnderline size={22} />
+                <span>Sublinhado</span>
+              </button>
+              <button
+                title="Citação (alt+q)"
+                onClick={() => CustomEditor.toggleQuoteBlock(editor)}
+              >
+                <Quotes size={22} weight={"fill"} />
+                <span>Citação</span>
+              </button>
+              <button
+                title="Código (alt+c)"
+                onClick={() => CustomEditor.toggleCodeBlock(editor)}
+              >
+                <CodeSimple size={22} />
+                <span>Código</span>
+              </button>
+              <button
+                title="Lista (alt+z)"
+                onClick={() => CustomEditor.toggleListBlock(editor)}
+              >
+                <ListDashes size={22} />
+                <span>Lista</span>
+              </button>
+              <button
+                title="Justificar (alt+j)"
+                onClick={() => CustomEditor.setTextAlign(editor, "justify")}
+              >
+                <TextAlignJustify size={22} />
+                <span>Justificar</span>
+              </button>
+              <button
+                title="Centralizar (alt+e)"
+                onClick={() => CustomEditor.setTextAlign(editor, "center")}
+              >
+                <TextAlignCenter size={22} />
+                <span>Centralizar</span>
+              </button>
+              <button
+                title="Alinhar à esquerda (alt+l)"
+                onClick={() => CustomEditor.setTextAlign(editor, "left")}
+              >
+                <TextAlignLeft size={22} />
+                <span>Alinhar à esquerda</span>
+              </button>
+              <button
+                title="Alinhar à direita (alt+r)"
+                onClick={() => CustomEditor.setTextAlign(editor, "right")}
+              >
+                <TextAlignRight size={22} />
+                <span>Alinhar à direita</span>
+              </button>
+            </div>
+            <Slate
+              editor={editor}
+              value={initialValue}
+              onChange={(value) => saveContent(value)}
             >
-              <TextH size={22} />
-              <span>Título</span>
-            </button>
-            <button
-              title="Negrito (alt+b)"
-              onClick={() => CustomEditor.toggleBoldMark(editor)}
-            >
-              <TextBolder size={22} />
-              <span>Negrito</span>
-            </button>
-            <button
-              title="Itálico (alt+i)"
-              onClick={() => CustomEditor.toggleItalicMark(editor)}
-            >
-              <TextItalic size={22} />
-              <span>Itálico</span>
-            </button>
-            <button
-              title="Sublinhado (alt+u)"
-              onClick={() => CustomEditor.toggleUnderlineMark(editor)}
-            >
-              <TextUnderline size={22} />
-              <span>Sublinhado</span>
-            </button>
-            <button
-              title="Citação (alt+q)"
-              onClick={() => CustomEditor.toggleQuoteBlock(editor)}
-            >
-              <Quotes size={22} weight={"fill"} />
-              <span>Citação</span>
-            </button>
-            <button
-              title="Código (alt+c)"
-              onClick={() => CustomEditor.toggleCodeBlock(editor)}
-            >
-              <CodeSimple size={22} />
-              <span>Código</span>
-            </button>
-            <button
-              title="Lista (alt+z)"
-              onClick={() => CustomEditor.toggleListBlock(editor)}
-            >
-              <ListDashes size={22} />
-              <span>Lista</span>
-            </button>
-            <button
-              title="Justificar (alt+j)"
-              onClick={() => CustomEditor.setTextAlign(editor, "justify")}
-            >
-              <TextAlignJustify size={22} />
-              <span>Justificar</span>
-            </button>
-            <button
-              title="Centralizar (alt+e)"
-              onClick={() => CustomEditor.setTextAlign(editor, "center")}
-            >
-              <TextAlignCenter size={22} />
-              <span>Centralizar</span>
-            </button>
-            <button
-              title="Alinhar à esquerda (alt+l)"
-              onClick={() => CustomEditor.setTextAlign(editor, "left")}
-            >
-              <TextAlignLeft size={22} />
-              <span>Alinhar à esquerda</span>
-            </button>
-            <button
-              title="Alinhar à direita (alt+r)"
-              onClick={() => CustomEditor.setTextAlign(editor, "right")}
-            >
-              <TextAlignRight size={22} />
-              <span>Alinhar à direita</span>
-            </button>
-          </div>
-          <Slate
-            editor={editor}
-            value={initialValue}
-            onChange={(value) => saveContent(value)}
-          >
-            <Editable
-              onKeyDown={(e) => handleKeyDown(e)}
-              renderElement={renderElement}
-              renderLeaf={renderLeaf}
-            />
-          </Slate>
-        </main>
+              <Editable
+                onKeyDown={(e) => handleKeyDown(e)}
+                renderElement={renderElement}
+                renderLeaf={renderLeaf}
+              />
+            </Slate>
+          </main>
+        </div>
       </div>
     </>
   );
